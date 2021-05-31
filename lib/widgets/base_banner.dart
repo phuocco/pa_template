a9cefe4fd1a31a3a05214d6792160c82e14b354d
Phuoc Nguyen <vodanh.2901@gmail.com>
2021-05-31 14:10:39 +0700

    hide banner ad in more app page

--- lib/widgets/base_banner.dart
+++ lib/widgets/base_banner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:pa_template/app/modules/home_module/home_controller.dart';
 import 'package:pa_template/constants/const_drawer.dart';
 
 import '../controllers/ads_controller.dart';
@@ -8,10 +9,10 @@ import '../functions/util_functions.dart';
 
 class BaseBanner extends StatelessWidget {
   final AdsController controller = Get.find();
-
+  final HomeController homeController = Get.find();
   @override
   Widget build(BuildContext context) {
-    return Obx(() => controller.list.length == 0
+    return Obx(() => controller.list.length == 0 || homeController.selectingPageNew.value == 'More App Page'
         ? Container(
             height: 0,
             width: double.infinity,
